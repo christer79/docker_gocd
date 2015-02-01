459d6bb72ac489940346172c14c0b0297ec1e032
Christer Eriksson <rockarasfett@gmail.com>
2015-02-01 21:48:57 +0100

    start-agent.sh now tails the two proper log-files

--- start-agent.sh
+++ start-agent.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/share/go-agent/agent.sh && tail -F /var/log/go-agent/go-agent.log
+/usr/share/go-agent/agent.sh && tail -F /var/log/go-agent/go-agent-launcher.log /var/log/go-agent/go-agent-bootstrapper.log
