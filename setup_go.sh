7cf68a1e90218d92a569aee716dfdb442accf755
Christer Eriksson <rockarasfett@gmail.com>
2015-02-02 21:11:51 +0100

    Mapping a volume for backups when starting the container

--- setup_go.sh
+++ setup_go.sh
@@ -3,7 +3,7 @@
 docker build -t gocd .
 
 docker rm -f go_server
-docker run --name go_server --publish 8153:8153 --publish 8154:8154 -d gocd /root/start-server.sh 
+docker run --name go_server --publish 8153:8153 --publish 8154:8154 --volume $(pwd)/backups:/var/lib/go-server/artifacts/serverBackups -d gocd /root/start-server.sh 
 docker exec -d go_server /root/start-agent.sh 
 
 #docker rm -f go_agent_1 go_agent_2 go_agent_3
